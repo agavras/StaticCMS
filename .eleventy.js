f1aa171a28ac29a4dcad94a8c225f6eb3ec7c4f8
agavras <math.falze@gmail.com>
2022-07-09 16:18:15 +0200

    11tyfied-static-pages

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        },
+        // templateFormats: ['md', 'njk', 'html'],
+        // markdownTemplateEngine: 'njk',
+        // htmlTemplateEngine: 'njk',
+        // dataTemplateEngine: 'njk',
+    };
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# StaticCMS
+
+Testing static cms possibility using eleventy
+
+
+## Tutorial
+
+You can [watch this video](https://youtu.be/4wD00RT6d-g) to see how Kevin uses Eleventy, Netlify, and Netlify CMS to create a full-featured blog site built entirely with static files.
